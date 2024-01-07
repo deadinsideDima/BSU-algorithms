f8803d23403f6084bdc392d10866720b89875a43
deadinsideDima <137908546+deadinsideDima@users.noreply.github.com>
2024-01-07 16:00:35 +0300

    add solves

---
+++ 0.0/task0.0.cpp
@@ -0,0 +1,34 @@
+﻿// task0.0.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	set<long long> tree;
+
+	string line;
+	ifstream in("input.txt");
+	if (in.is_open()) {
+		while (getline(in, line)) {
+			tree.insert(stoi(line));
+		}
+	}
+	in.close();
+	long long res = 0;
+	for (long long x : tree) {
+		res += x;
+	}
+	ofstream out;
+	out.open("output.txt");
+	if (out.is_open()) {
+		out << res << endl;
+	}
+	out.close();
+	return 0;
+}
